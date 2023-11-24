cd45e1d532521c004feac42e939e72deada16187
Plinio Umezaki <plinio.umezaki@evoluservices.com>
2023-11-24 10:57:44 -0300

    Adiciona requirements.txt e retorna resposta no formato HTML

--- api.py
+++ api.py
@@ -21,10 +21,10 @@ app.config['UPLOAD_FOLDER'] =UPLOAD_FOLDER
 
 client = OpenAI(
     # organization='org-11Qcu80v0wYOPv04D1cK73JA',
-    api_key='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'
+    api_key='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'
 )
 
-assistant_id = 'asst_ZuilBWsi6wqjafhhcFMrydpd'
+assistant_id = 'asst_x7Yczc07pp7Hzm9HMaxiQ7N1'
 
 def _create_thread():
     return client.beta.threads.create()
@@ -66,7 +66,7 @@ def send_prompt(thread_id):
         thread_id=thread_id,
         role='user',
         # content=f"Which color is warm, red or blue?"
-        content=prompt
+        content=prompt + "Responda em HTML e deixe o texto limpo"
     )
     print(f"Message: {message.content}")
 
@@ -90,6 +90,7 @@ def send_prompt(thread_id):
     for message in reversed(messages.data):
         message_value = message.role + ": " + message.content[0].text.value
         response_messages.append(message_value)
+    print("Response Message")
     print(response_messages)
 
     # finalResponse = response.data['messages']
@@ -149,7 +150,8 @@ def analyze_file():
         message_value = message.role + ": " + message.content[0].text.value
         response_messages.append(message_value)
 
-    print(response_messages)
+    print('Response Message:' + response_messages)
+
 
     return jsonify({'response': response_messages[-1]})
 
